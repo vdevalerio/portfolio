0761d7e05057e8033591141fc0b4e6db139d094e
Marcos Valério <marcosvaleriox12@hotmail.com>
2024-03-16 20:46:37 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Personal Portfolio</title>
+</head>
+<body>
+
+  <h1>Who am I ?</h1>
+  <p> Marcos Valério de Carvalho Cardoso Júnior</p>
+  <p>This site is intended to present to myself and my professional profile to anyone interested.</p>
+
+</body>
+</html>
